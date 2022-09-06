6a72eac431789640ee3d151d9663871677b9637c
Will Opie <51498840+william-opie@users.noreply.github.com>
2022-09-06 09:47:12 -0500

    Added additional search options for subsequent patch checks
    After completing the initial patch check, there are now two additional options:
    - Check another server from a different date than originally queried
    - Check same server (from last query) from a different date

--- WindowsRemotePatchChecker.ps1
+++ WindowsRemotePatchChecker.ps1
@@ -3,7 +3,7 @@ Write-Host "Enter the name of the server you wish to check updates for:"
 $Server = Read-Host
 
 Write-Host "`nEnter the date you wish to check updates for. Date must be written in the following format: Month/Day/Year `n(Ex.: 8/31/2022, 12/2/2021)"
-$CheckDate = Read-Host
+$CheckDate = Read-Host "`nPlease enter date"
 
 While($true){
     #Clear-Host
@@ -12,19 +12,41 @@ While($true){
     Write-Host " on " -NoNewLine
     Write-Host "$Server" -ForegroundColor Green -NoNewLine
     Write-Host "...`n`n" -NoNewLine
-    Invoke-Command -ComputerName $Server -ScriptBlock {Get-Hotfix} | Where{$_.InstalledOn -ge "$CheckDate"} | Sort InstalledOn
+    Invoke-Command -ComputerName $Server -ScriptBlock {Get-Hotfix} | Where{$_.InstalledOn -ge "$CheckDate"} | Sort InstalledOn | Format-Table
 
     DO{
-        Write-Host "`nCheck another server? Y/N"
-        $LoopBack = Read-Host
+        Write-Host "`nCheck another server?`n" -ForegroundColor Red -BackgroundColor Yellow
+        Write-Host "[1] Check another server from same date " -NoNewLine
+        Write-Host "($CheckDate)" -ForegroundColor Yellow
+        Write-Host "[2] Check another server from a different date"
+        Write-Host "[3] Check same server from a different date " -NoNewLine
+        Write-Host "($Server)" -ForegroundColor Green
+        Write-Host "[4] Exit script`n"
+        $LoopBack = Read-Host "Please enter your selection"
         Switch($LoopBack)
         {
-            Y{
+            1{
                 $Server = $Null
                 Write-Host "`nEnter the name of the server you wish to check updates for:"
                 $Server = Read-Host
             }
-            N{
+            2{
+                $Server = $Null
+                $CheckDate = $Null
+                Write-Host "Enter the name of the server you wish to check updates for:"
+                $Server = Read-Host
+
+                Write-Host "`nEnter the date you wish to check updates for. Date must be written in the following format: Month/Day/Year `n(Ex.: 8/31/2022, 12/2/2021)"
+                $CheckDate = Read-Host
+            }
+            3{
+                $CheckDate = $Null
+                Write-Host "`nEnter the date you wish to check updates for on " -NoNewLine
+                Write-Host "$Server" -NoNewLine -ForegroundColor Green
+                Write-Host ". `nDate must be written in the following format: Month/Day/Year `n(Ex.: 8/31/2022, 12/2/2021)"
+                $CheckDate = Read-Host "`nPlease enter date"
+            }
+            4{
                 Write-Host "`nExiting script..." -ForegroundColor Red
                 Start-Sleep -Seconds 3
                 Exit
@@ -33,5 +55,5 @@ While($true){
                 Write-Host "`nInvalid input. Try again" -BackgroundColor Red -ForegroundColor White
             }
         }
-    }Until($LoopBack -like "[y/n]")
+    }Until($LoopBack -like "[1/2/3/4]")
 }
